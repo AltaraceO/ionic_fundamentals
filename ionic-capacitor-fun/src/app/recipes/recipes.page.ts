e548863c83e9beae57d85b6deb5ecc896924e544
AltaraceO <orialtarace@gmail.com>
2022-06-10 18:41:15 +0300

    add image

--- ionic-capacitor-fun/src/app/recipes/recipes.page.ts
+++ ionic-capacitor-fun/src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'schnitzel',
+      image: 'https://unsplash.com/photos/flFd8L7_B3g',
+      ingredients: ['french fries', 'chicken', 'bread'],
+    },
+    {
+      id: 'r2',
+      title: 'pasta',
+      image: 'url',
+      ingredients: ['french pasta', 'tomato', 'basil'],
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
